664bb750940931430c7bb9df4fc6f53a9bf480b1
deanovision <rodeanfraser@gmail.com>
2019-02-28 16:31:52 -0500

    finished adding objects using the classes set up previously also tested all the methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,6 +26,8 @@ class Instructor extends Person {
     }
 }
 
+
+
 class Student extends Person {
     constructor(props){
         super(props)
@@ -33,17 +35,19 @@ class Student extends Person {
         this.className = props.className;
         this.favSubjects = props.favSubjects;
     }
-    listsSubjects(){
-        this.favSubjects.array.forEach(function(currentValue){
-            return this.favSubjects(currentValue);      
-            
-        });
+     listsSubjects(){
 
+        return (this.favSubjects.toString())           
+               
     }
     PRAssignment(subject){
+
         return `${this.name} has submitted a PR for ${subject}`;
     }
 }
+
+
+
 class ProjectManager extends Instructor{
     constructor(props){
         super(props)
@@ -57,3 +61,77 @@ class ProjectManager extends Instructor{
         return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
+
+////////////TESTING/////////////////
+
+const jim = new Person({
+    name: "Jim",
+    age: 30,
+    location: "Scranton, PA",
+    gender: "Male"
+})
+
+const pam = new Instructor({
+    name: "Pam",
+    age: 28,
+    location: "Scranton, PA",
+    gender: "Female",
+    specialty: "Drawing",
+    favLanguage: "Javascript",
+    catchPhrase: "Michael is an idiot"
+})
+
+const dwight = new Instructor({
+    name: "Dwight",
+    age: 32,
+    location: "Scranton, PA",
+    gender: "Male",
+    specialty: "Beet Farming",
+    favLanguage: "English",
+    catchPhrase: "I am assistant regional manager"
+})
+
+const michael = new Student({
+    name: "Michael",
+    age: 40,
+    location: "Scranton, PA",
+    gender: "Male",
+    specialty: "Middle Management",
+    favLanguage: "English",
+    catchPhrase: "I am a manager",
+    previousBackground: "Paper sales",
+    className: "Web18",
+    favSubjects: ["CSS", "HTML", "Javascript"]
+    
+})
+
+const david = new ProjectManager({
+    name: "David",
+    age: 35,
+    location: "Scranton, PA",
+    gender: "Male",
+    specialty: "Middle Management",
+    favLanguage: "English",
+    catchPhrase: "I am a PM",
+    previousBackground: "Paper sales",
+    className: "Web18",
+    favSubjects: ["CSS", "HTML", "Javascript"],
+    gradClassName: "Web 10",
+    favInstructor: "Josh"
+})
+
+
+
+//console.log(jim.speak());
+
+//console.log(pam.demo("CSS"));
+
+//console.log(dwight.grade(jim, "Bear Defense"))
+
+//console.log(michael.listsSubjects())
+
+//console.log(michael.PRAssignment("CSS"))
+
+//console.log(david.standUp("Web18"))
+
+//console.log(david.debugsCode(michael, "React"))
